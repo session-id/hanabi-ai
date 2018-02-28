b464eeed3a11a8d6cc1baa9a713d502ff0e40c1d
Vinson Luo <vluo@stanford.edu>
2018-02-28 12:00:13 -0800

    Add linear config params.

--- config.py
+++ config.py
@@ -21,8 +21,28 @@ class QL_Config(object):
 
 
 class LinearQL_Config(object):
-    # TODO: for Arthur
+    train_num_steps = 200       # number of steps for training
+    train_start = 0             # how many steps of observation before beginning training
+    print_freq = 5              # how often to print loss and other training statistics (training steps)
+
+    test_freq = 100             # how often to evaluate model (training steps)
+    test_num_episodes = 30      # how many episodes to test for
+
+    gpu = -1                    # which GPU to use. set to -1 to use CPU only
+
+    # TODO: make these unique per experiment
+    log_dir = 'logs/'           # path to directory for TensorBoard summaries
+    ckpt_dir = 'ckpts/'         # path to directory for checkpoints
+
+    gamma = 0.9                 # discount factor
+    lr = 0.01                   # learning rate
+    soft_epsilon = 0.01         # epsilon for e-greedy exploration
+    
+    batch_size = 20             # batch size
+
+    widths = []           # widths if you want to do mlp stuff
 
 
 class DeepQL_Config(object):
     # TODO: for Arthur
+    pass
