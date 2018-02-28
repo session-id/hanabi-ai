e76180609ca876f7d9dc062f5691d65aca47365a
Vinson Luo <vluo@stanford.edu>
2018-02-28 11:20:25 -0800

    Add untested implementation for linear_ql.

--- models/linear_ql.py
+++ models/linear_ql.py
@@ -10,4 +10,10 @@ class LinearQL_Model(QL_Model):
         - scope: str, name of scope
         '''
         # TODO: this is for you, Arthur!
-        raise NotImplementedError
+        h = self.placeholders['states']
+        num_actions = self.train_simulator.num_actions()
+        with tf.variable_scope(scope):
+            for width in widths:
+                h = tf.layers.dense(h, width, activation=tf.nn.relu())
+            q_values = tf.layers.dense(h, num_actions)
+        return q_values
