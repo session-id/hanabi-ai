ff9f42d0c2c8163b848cc76f1f18a53f59588947
Arthur Leonard Tsang <atsang2@rice05.stanford.edu>
2018-02-28 19:59:47 -0800

    fix bug in DQN, edit run.py to work with DQN

--- config.py
+++ config.py
@@ -50,11 +50,11 @@ class LinearQL_Config(object):
 
 
 class DeepQL_Config(object):
-    train_num_steps = 200       # number of steps for training
+    train_num_steps = 50000       # number of steps for training
     train_start = 0             # how many steps of observation before beginning training
-    print_freq = 5              # how often to print loss and other training statistics (training steps)
+    print_freq = 200              # how often to print loss and other training statistics (training steps)
 
-    test_freq = 100             # how often to evaluate model (training steps)
+    test_freq = 1000             # how often to evaluate model (training steps)
     test_num_episodes = 30      # how many episodes to test for
 
     gpu = -1                    # which GPU to use. set to -1 to use CPU only
@@ -62,7 +62,7 @@ class DeepQL_Config(object):
     log_dir = 'deep-logs/'           # path to directory for TensorBoard summaries
     ckpt_dir = 'deep-ckpts/'         # path to directory for checkpoints
 
-    gamma = 0.9                 # discount factor
+    gamma = 0.99                 # discount factor
     lr = 0.01                   # learning rate
     soft_epsilon = 0.01         # epsilon for e-greedy exploration
     
--- models/deep_ql.py
+++ models/deep_ql.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 class DQL_Model(QL_Model):
 
-    def _get_q_values_op(self, scope):
+    def _get_q_values_op(self, states, scope):
         '''
         Args
         - state: tf.Tensor, shape [batch_size, state_dim]
@@ -19,6 +19,6 @@ class DQL_Model(QL_Model):
         num_actions = self.train_simulator.get_num_actions()
         with tf.variable_scope(scope):
             for width in self.config.widths:
-                h = tf.layers.dense(h, width, activation=tf.nn.relu())
+                h = tf.layers.dense(h, width, activation=tf.nn.relu)
             q_values = tf.layers.dense(h, num_actions)
         return q_values
